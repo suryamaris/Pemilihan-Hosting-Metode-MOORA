9a6707c5615486671a429cd81caa6db70e4420d1
suryamaris <smarispanensa@gmail.com>
2021-08-13 02:04:32 +0700

    Tambah data dan Hapus

--- application/models/data_model.php
+++ application/models/data_model.php
@@ -1,43 +1,43 @@
 <?php
-defined('BASEPATH') OR exit('No direct script acces allowed');
+defined('BASEPATH') or exit('No direct script acces allowed');
 
 
 class Data_model extends CI_Model
 {
-	
+
 	public function getHosting($table)
 	{
-		$res=$this->db->get($table);
+		$res = $this->db->get($table);
 		return $res->result_array();
 	}
 
-	public function getWhere($table,$data)
+	public function getWhere($table, $data)
 	{
-		$res=$this->db->get_where($table,$data);
+		$res = $this->db->get_where($table, $data);
 		return $res->result_array();
 	}
 
-	public function Insert($table,$data)
+	public function Insert($table, $data)
 	{
-		$res=$this->db->insert($table,$data);
+		$res = $this->db->insert($table, $data);
 		return $res;
 	}
 
-	public function Update($table,$data,$where)
+	public function Update($table, $data, $where)
 	{
-		$res=$this->db->update($table,$data,$where);
+		$res = $this->db->update($table, $data, $where);
 		return $res;
 	}
 
-	public function Delete($table,$where)
+	public function Delete($table, $where)
 	{
-		$res=$this->db->delete($table,$where);
+		$res = $this->db->delete($table, $where);
 		return $res;
 	}
 
 	public function getHostingLimit($limit, $start)
 	{
-		return $this->db->get('list', $limit, $start)->result_array();	 
+		return $this->db->get('list', $limit, $start)->result_array();
 	}
 	public function countAllHosting()
 	{
@@ -46,226 +46,253 @@ class Data_model extends CI_Model
 	public function Input()
 	{
 		$this->load->view('form_add');
-		$fitur=array('0'=>'');
-		if($this->input->post('wordpress')=='ya')
-		$fitur = array('wp'=>'Paket Wordpress');
-		if($this->input->post('litespeed')=='ya')
-		$fitur = array('lite'=>'Litespeed');
-		if($this->input->post('fiturSSD')=='ya')
-		$fitur += array('fiturSSD'=>'Penyimpanan SSD');
-		if($this->input->post('auto')=='Harian')
-		$fitur += array('auto'=>'Backup Harian');
-		elseif($this->input->post('auto')=='Mingguan')
-		$fitur += array('auto'=>'Backup Mingguan');
-		elseif($this->input->post('auto')=='Bulanan')
-		$fitur += array('auto'=>'Backup Bulanan');
-		if($this->input->post('domain')=='Terbatas')
-		$fitur += array('domain'=>'Free Domain terbatas');
-		elseif($this->input->post('domain')=='Unlimited')
-		$fitur += array('domain'=>'Free Domain Unlimited');
-		if($this->input->post('add')=='Terbatas')
-		$fitur += array('add'=>'Addon Domain terbatas');
-		elseif($this->input->post('add')=='Unlimited')
-		$fitur += array('add'=>'Addon Domain Unlimited');
-		if($this->input->post('email')=='Terbatas')
-		$fitur += array('email'=>'Email  terbatas');
-		elseif($this->input->post('email')=='Unlimited')
-		$fitur += array('email'=>'Email  Unlimited');
+		$fitur = array('0' => '');
+		if ($this->input->post('wordpress') == 'ya')
+			$fitur = array('wordpress' => 'Paket Wordpress');
+		if ($this->input->post('litespeed') == 'ya')
+			$fitur = array('lite' => 'Litespeed');
+		if ($this->input->post('fiturSSD') == 'ya')
+			$fitur += array('fiturSSD' => 'Penyimpanan SSD');
+		$fitur += array('auto' => 'Backup ' . $this->input->post('auto'));
+		if ($this->input->post('domain') != 'tidak')
+			$fitur += array('domain' => 'Free ' . $this->input->post('domain') . ' Domain');
+		if ($this->input->post('email') != 'tidak')
+			$fitur += array('email' => 'Free ' . $this->input->post('email') . ' Email');
+		if ($this->input->post('add') != 'tidak')
+			$fitur += array('add' => 'Free ' . $this->input->post('add') . ' Addon Domain');
+		if ($this->input->post('sub') != 'tidak')
+			$fitur += array('sub' => 'Free ' . $this->input->post('sub') . ' Sub Domain');
 		$myFitur = implode(', ', $fitur);
-			
-		$bandwith= $this->input->post('bandwith');
-		if($bandwith=='0')
-		$bandwith='Unlimited';
-		else
-		$bandwith= $bandwith+ ' GB';
-		
-		$ssd= $this->input->post('ssd');
-		if($ssd=='0')
-		$ssd='Unlimited';
 
-		$inode= $this->input->post('inode');
-		if($inode=='0')
-		$inode='Unlimited';
-		
-		$data= array('id' =>$this->input->post('id')
-			,'nama' =>$this->input->post('nama')
-			,'penyedia' =>$this->input->post('penyedia')
-			,'uptime' =>$this->input->post('uptime')
-			,'ssd' =>$ssd
-			,'ram' =>$this->input->post('ram')
-			,'cpu' =>$this->input->post('cpu')
-			,'bandwith' =>$bandwith
-			,'inode' =>$inode
-			,'support' =>$this->input->post('support')
-			,'wordpress' =>$this->input->post('wordpress')
-			,'fitur' =>$myFitur
-			,'garansi' =>$this->input->post('garansi')
-			,'harga' =>$this->input->post('harga')
-			,'keamanan' =>$this->input->post('Keamanan'));
+		$bandwith = $this->input->post('bandwidth');
+		if ($bandwith != 'Unlimited')
+			$bandwith = $bandwith . ' GB';
+
+		$ssd = $this->input->post('ssd');
+		if ($ssd == '0')
+			$ssd = 'Unlimited';
+
+		$inode = $this->input->post('inode');
+		if ($inode == '0')
+			$inode = 'Unlimited';
+
+		$data = array(
+			'id' => $this->input->post('id'),
+			'nama' => $this->input->post('nama'),
+			'penyedia' => $this->input->post('penyedia'),
+			'uptime' => $this->input->post('uptime'),
+			'ssd' => $ssd, 'ram' => $this->input->post('ram'),
+			'cpu' => $this->input->post('cpu'),
+			'bandwidth' => $bandwith,
+			'inode' => $inode,
+			'support' => $this->input->post('support'),
+			'fitur' => $myFitur,
+			'garansi' => $this->input->post('garansi'),
+			'harga' => $this->input->post('harga'),
+			'keamanan' => $this->input->post('Keamanan')
+		);
 		return $data;
 	}
-	public function inputNilai(){
-		$this->load->view('form_add');
-		$fitur = 0;
-		if($this->input->post('wordpress')=='ya')
-		$fitur+=1;
-		if($this->input->post('litespeed')=='ya')
-		$fitur+=3;
-		if($this->input->post('ssd')=='ya')
-		$fitur+=1;
-		if($this->input->post('auto')=='Harian')
-		$fitur+=3;
-		elseif($this->input->post('auto')=='Mingguan')
-		$fitur+=2;
-		elseif($this->input->post('auto')=='Bulanan')
-		$fitur+=1;
-		if($this->input->post('domain')=='Terbatas')
-		$fitur+=1;
-		elseif($this->input->post('domain')=='Unlimited')
-		$fitur+=3;
-
-
-		$ssd= $this->input->post('ssd');
-		if($ssd == 0)
-		$ssd=1;
-		$inode= $this->input->post('inode');
-		if($inode== 0)
-		$inode=2000000;
+	public function inputNilai($fitur)
+	{
+		$ssd = $this->input->post('ssd');
+		if ($ssd == 0)
+			$ssd = 1;
+		$inode = $this->input->post('inode');
+		if ($inode == 0)
+			$inode = 2000000;
 		$bandwith = $this->input->post('bandwith');
-		if($bandwith==0)
-		$bandwith=1500;
+		if ($bandwith == 0)
+			$bandwith = 1500;
 		$support = $this->input->post('support');
-		if($support=='Fast Support 24/7')
-		$support=5;
+		if ($support == 'Fast Support 24/7')
+			$support = 5;
 		else
-		$support=3;
-	$dataNilai = array('id' => $this->input->post('id') 
-					,'keamanan' => $this->input->post('jumlah')
-					,'uptime' =>$this->input->post('uptime')
-					,'ssd' =>$ssd
-					,'ram' =>$this->input->post('ram')
-					,'cpu' =>$this->input->post('cpu')
-					,'bandwith' =>$bandwith
-					,'inode' =>$inode
-					,'support' =>$support
-					,'fitur' =>$fitur
-					,'garansi' =>$this->input->post('garansi')
-					,'harga' =>$this->input->post('harga')
-				);
+			$support = 3;
+		$dataNilai = array(
+			'id' => $this->input->post('id'),
+			'keamanan' => $this->input->post('jumlah'),
+			'uptime' => $this->input->post('uptime'),
+			'ssd' => $ssd, 'ram' => $this->input->post('ram'), 'cpu' => $this->input->post('cpu'), 'bandwidth' => $bandwith, 'inode' => $inode, 'support' => $support, 'fitur' => $fitur, 'garansi' => $this->input->post('garansi'), 'harga' => $this->input->post('harga')
+		);
 		return $dataNilai;
 	}
 
-	public function update_data(){
-		$this->load->view('form_add');
-		$fitur=array('0'=>'');
-		if($this->input->post('wordpress')=='ya')
-		$fitur = array('wp'=>'Paket Wordpress');
-		if($this->input->post('litespeed')=='ya')
-		$fitur = array('lite'=>'Litespeed');
-		if($this->input->post('fiturSSD')=='ya')
-		$fitur += array('fiturSSD'=>'Penyimpanan SSD');
-		if($this->input->post('auto')=='Harian')
-		$fitur += array('auto'=>'Backup Harian');
-		elseif($this->input->post('auto')=='Mingguan')
-		$fitur += array('auto'=>'Backup Mingguan');
-		elseif($this->input->post('auto')=='Bulanan')
-		$fitur += array('auto'=>'Backup Bulanan');
-		if($this->input->post('domain')=='Terbatas')
-		$fitur += array('domain'=>'Free Domain terbatas');
-		elseif($this->input->post('domain')=='Unlimited')
-		$fitur += array('domain'=>'Free Domain Unlimited');
-		if($this->input->post('add')=='Terbatas')
-		$fitur += array('add'=>'Addon Domain terbatas');
-		elseif($this->input->post('add')=='Unlimited')
-		$fitur += array('add'=>'Addon Domain Unlimited');
-		if($this->input->post('email')=='Terbatas')
-		$fitur += array('email'=>'Email  terbatas');
-		elseif($this->input->post('email')=='Unlimited')
-		$fitur += array('email'=>'Email  Unlimited');
-		$myFitur = implode(', ', $fitur);
 
-			
-		$bandwith= $this->input->post('bandwith');
-		if($bandwith=='1500')
-		$bandwith='Unlimited';
-		else
-		$bandwith= $bandwith+ ' GB';
-		
-		$ssd= $this->input->post('ssd');
-		if($ssd=='0')
-		$ssd='Unlimited';
+	public function inputFitur()
+	{
+		$nilaiFitur = 0;
+		// wordpress
+		$wordpress = $this->input->post('wordpress');
+		if ($wordpress == 'ya') {
+			$nilaiFitur += 1;
+			$wordpress = 'Wordpress';
+		}
+		// SSL
+		$freessl = $this->input->post('freessl');
+		if ($freessl == 'ya') {
+			$nilaiFitur += 1;
+			$freessl = 'Free SSL';
+		}
+		// Litespeed
+		$litespeed = $this->input->post('litespeed');
+		if ($litespeed == 'ya') {
+			$nilaiFitur += 1;
+			$litespeed = 'Litespeed';
+		}
+		// Auto Backup
+		$auto = $this->input->post('auto');
+		if ($auto == 'Harian') {
+			$nilaiFitur += 3;
+		} elseif ($auto == 'Mingguan') {
+			$nilaiFitur += 2;
+		} elseif ($auto == 'Bulanan') {
+			$nilaiFitur += 1;
+		} else {
+			$nilaiFitur += 0;
+		}
+		// Domain Gratis
+		$domain = $this->input->post('domain');
+		if ($domain == 'Unlimited') {
+			$nilaiFitur += 3;
+		} elseif ($domain == 'tidak') {
+			$nilaiFitur += 0;
+		} else {
+			$nilaiFitur += 1;
+		}
+		// Email Gratis
+		$email = $this->input->post('email');
+		if ($email == 'Unlimited') {
+			$nilaiFitur += 3;
+		} elseif ($email == 'tidak') {
+			$nilaiFitur += 0;
+		} else {
+			$nilaiFitur += 1;
+		}
+		// Addon Domain
+		$add = $this->input->post('add');
+		if ($add == 'Unlimited') {
+			$nilaiFitur += 3;
+		} elseif ($add == 'tidak') {
+			$nilaiFitur += 0;
+		} else {
+			$nilaiFitur += 1;
+		}
+		// Sub Domain
+		$sub = $this->input->post('sub');
+		if ($sub == 'Unlimited') {
+			$nilaiFitur += 3;
+		} elseif ($sub == 'tidak') {
+			$nilaiFitur += 0;
+		} else {
+			$nilaiFitur += 1;
+		}
 
-		$inode= $this->input->post('inode');
-		if($inode=='0')
-		$inode='Unlimited';
-		
-		$data= array('id' =>$this->input->post('id')
-			,'nama' =>$this->input->post('nama')
-			,'penyedia' =>$this->input->post('penyedia')
-			,'uptime' =>$this->input->post('uptime')
-			,'ssd' =>$ssd
-			,'ram' =>$this->input->post('ram')
-			,'cpu' =>$this->input->post('cpu')
-			,'bandwith' =>$bandwith
-			,'inode' =>$inode
-			,'support' =>$this->input->post('support')
-			,'wordpress' =>$this->input->post('wordpress')
-			,'fitur' =>$myFitur
-			,'garansi' =>$this->input->post('garansi')
-			,'harga' =>$this->input->post('harga')
-			,'keamanan' =>$this->input->post('Keamanan'));
+		$data = array(
+			'id' => $this->input->post('id'),
+			'wordpress' => $wordpress,
+			'freessl' => $freessl,
+			'litespeed' => $litespeed,
+			'auto' => $auto,
+			'domain' => $domain,
+			'email' => $email,
+			'add' => $add,
+			'sub' => $sub,
+			'nilai' => $nilaiFitur
+		);
 		return $data;
 	}
 
-	public function updateNilai(){
+	public function update_data()
+	{
+		$this->load->view('form_add');
+		$fitur = array('0' => '');
+		if ($this->input->post('wordpress') == 'ya')
+			$fitur = array('wp' => 'Paket Wordpress');
+		if ($this->input->post('litespeed') == 'ya')
+			$fitur = array('lite' => 'Litespeed');
+		if ($this->input->post('fiturSSD') == 'ya')
+			$fitur += array('fiturSSD' => 'Penyimpanan SSD');
+		if ($this->input->post('auto') == 'Harian')
+			$fitur += array('auto' => 'Backup Harian');
+		elseif ($this->input->post('auto') == 'Mingguan')
+			$fitur += array('auto' => 'Backup Mingguan');
+		elseif ($this->input->post('auto') == 'Bulanan')
+			$fitur += array('auto' => 'Backup Bulanan');
+		if ($this->input->post('domain') == 'Terbatas')
+			$fitur += array('domain' => 'Free Domain terbatas');
+		elseif ($this->input->post('domain') == 'Unlimited')
+			$fitur += array('domain' => 'Free Domain Unlimited');
+		if ($this->input->post('add') == 'Terbatas')
+			$fitur += array('add' => 'Addon Domain terbatas');
+		elseif ($this->input->post('add') == 'Unlimited')
+			$fitur += array('add' => 'Addon Domain Unlimited');
+		if ($this->input->post('email') == 'Terbatas')
+			$fitur += array('email' => 'Email  terbatas');
+		elseif ($this->input->post('email') == 'Unlimited')
+			$fitur += array('email' => 'Email  Unlimited');
+		$myFitur = implode(', ', $fitur);
+
+
+		$bandwith = $this->input->post('bandwith');
+		if ($bandwith == '1500')
+			$bandwith = 'Unlimited';
+		else
+			$bandwith = $bandwith + ' GB';
+
+		$ssd = $this->input->post('ssd');
+		if ($ssd == '0')
+			$ssd = 'Unlimited';
+
+		$inode = $this->input->post('inode');
+		if ($inode == '0')
+			$inode = 'Unlimited';
+
+		$data = array(
+			'id' => $this->input->post('id'), 'nama' => $this->input->post('nama'), 'penyedia' => $this->input->post('penyedia'), 'uptime' => $this->input->post('uptime'), 'ssd' => $ssd, 'ram' => $this->input->post('ram'), 'cpu' => $this->input->post('cpu'), 'bandwith' => $bandwith, 'inode' => $inode, 'support' => $this->input->post('support'), 'wordpress' => $this->input->post('wordpress'), 'fitur' => $myFitur, 'garansi' => $this->input->post('garansi'), 'harga' => $this->input->post('harga'), 'keamanan' => $this->input->post('Keamanan')
+		);
+		return $data;
+	}
+
+	public function updateNilai()
+	{
 		$this->load->view('form_edit');
 		$fitur = 0;
-		if($this->input->post('wordpress')=='ya')
-		$fitur+=1;
-		if($this->input->post('litespeed')=='ya')
-		$fitur+=3;
-		if($this->input->post('ssd')=='ya')
-		$fitur+=1;
-		if($this->input->post('auto')=='Harian')
-		$fitur+=3;
-		elseif($this->input->post('auto')=='Mingguan')
-		$fitur+=2;
-		elseif($this->input->post('auto')=='Bulanan')
-		$fitur+=1;
-		if($this->input->post('domain')=='Terbatas')
-		$fitur+=1;
-		elseif($this->input->post('domain')=='Unlimited')
-		$fitur+=3;
+		if ($this->input->post('wordpress') == 'ya')
+			$fitur += 1;
+		if ($this->input->post('litespeed') == 'ya')
+			$fitur += 3;
+		if ($this->input->post('ssd') == 'ya')
+			$fitur += 1;
+		if ($this->input->post('auto') == 'Harian')
+			$fitur += 3;
+		elseif ($this->input->post('auto') == 'Mingguan')
+			$fitur += 2;
+		elseif ($this->input->post('auto') == 'Bulanan')
+			$fitur += 1;
+		if ($this->input->post('domain') == 'Terbatas')
+			$fitur += 1;
+		elseif ($this->input->post('domain') == 'Unlimited')
+			$fitur += 3;
 
 
-		$ssd= $this->input->post('ssd');
-		if($ssd == 0)
-		$ssd=1;
-		$inode= $this->input->post('inode');
-		if($inode== 0)
-		$inode=2000000;
+		$ssd = $this->input->post('ssd');
+		if ($ssd == 0)
+			$ssd = 1;
+		$inode = $this->input->post('inode');
+		if ($inode == 0)
+			$inode = 2000000;
 		$bandwith = $this->input->post('bandwith');
-		if($bandwith==0)
-		$bandwith=1500;
+		if ($bandwith == 0)
+			$bandwith = 1500;
 		$support = $this->input->post('support');
-		if($support=='Fast Support 24/7')
-		$support=5;
+		if ($support == 'Fast Support 24/7')
+			$support = 5;
 		else
-		$support=3;
-	$dataNilai = array('id' => $this->input->post('id') 
-					,'keamanan' => $this->input->post('jumlah')
-					,'uptime' =>$this->input->post('uptime')
-					,'ssd' =>$ssd
-					,'ram' =>$this->input->post('ram')
-					,'cpu' =>$this->input->post('cpu')
-					,'bandwith' =>$bandwith
-					,'inode' =>$inode
-					,'support' =>$support
-					,'fitur' =>$fitur
-					,'garansi' =>$this->input->post('garansi')
-					,'harga' =>$this->input->post('harga')
-				);
+			$support = 3;
+		$dataNilai = array(
+			'id' => $this->input->post('id'), 'keamanan' => $this->input->post('jumlah'), 'uptime' => $this->input->post('uptime'), 'ssd' => $ssd, 'ram' => $this->input->post('ram'), 'cpu' => $this->input->post('cpu'), 'bandwith' => $bandwith, 'inode' => $inode, 'support' => $support, 'fitur' => $fitur, 'garansi' => $this->input->post('garansi'), 'harga' => $this->input->post('harga')
+		);
 		return $dataNilai;
 	}
-	
-}?>
+}
